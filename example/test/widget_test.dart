e46ad6e972fcfd05adc44b32e64d79a7efff2a41
7c00 <i@7c00.cc>
2022-05-20 10:03:42 +0800

    Add flutter lints

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -11,6 +11,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Widget test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(const App());
   });
 }
